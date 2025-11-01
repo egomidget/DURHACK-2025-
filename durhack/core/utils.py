9a9c14337b64c3080491fab56fed0bcefc6d6b8e
Sarah <sarifishh@yahoo.com>
2025-11-01 16:48:08 +0000

    i made utils.py and wrote in comments my plan

---
+++ durhack/core/utils.py
@@ -0,0 +1,11 @@
+'''python manage.py shell 
+
+import utils 
+
+function(jfkjkr)'''
+
+#get the ai 
+#get inputs.. get all the inputs from a question and put them as one input paragraph like get one vector embed for this?
+#get the embeds
+#compare the embeds to get the values of the pairs
+#optimise best pairs 
